0d0b97fe944c4295f31dac37859fc2f554e0d04a
Matt Schuchard <grapheneowns@gmail.com>
2021-04-13 14:55:46 -0400

    updates to module

--- module/CMakeDetermineGOCompiler.cmake
+++ module/CMakeDetermineGOCompiler.cmake
@@ -1,9 +1,13 @@
-# determine the compiler to use for Go programs
+# determine the compiler to use for GO programs
 #
 # Sets the following variables:
 #   CMAKE_GO_COMPILER
-#   CMAKE_COMPILER_IS_GNUGo
+#   CMAKE_COMPILER_IS_GNUGO
 #   CMAKE_GO_LINKER
+
+if(NOT CMAKE_GO_COMPILER_NAMES)
+  set(CMAKE_GO_COMPILER_NAMES GO)
+endif()
 
 IF(NOT CMAKE_GO_COMPILER)
   find_program(CMAKE_GO_COMPILER
@@ -14,13 +18,13 @@ IF(NOT CMAKE_GO_COMPILER)
     /usr/bin
     /usr/local/bin
   )
-  MESSAGE("Go Compiler at ${CMAKE_GO_COMPILER} used for compilation.")
+  MESSAGE("GO Compiler at ${CMAKE_GO_COMPILER} used for compilation.")
 ENDIF(NOT CMAKE_GO_COMPILER)
 MARK_AS_ADVANCED(CMAKE_GO_COMPILER)
 
 IF(NOT CMAKE_GO_COMPILER_ID_RUN)
   SET(CMAKE_GO_COMPILER_ID_RUN 1)
-  SET(CMAKE_COMPILER_IS_GNUGo 1)
+  SET(CMAKE_COMPILER_IS_GNUGO 1)
   SET(CMAKE_GO_COMPILER_ID "GNU")
   #TODO
   SET(CMAKE_GO_PLATFORM_ID "Linux")
@@ -35,7 +39,7 @@ IF(NOT CMAKE_GO_LINKER)
     /usr/bin
     /usr/local/bin
   )
-  MESSAGE("Go Linker at ${CMAKE_GO_LINKER} used for linking.")
+  MESSAGE("GO Linker at ${CMAKE_GO_LINKER} used for linking.")
 ENDIF(NOT CMAKE_GO_LINKER)
 MARK_AS_ADVANCED(CMAKE_GO_LINKER)
 
@@ -44,11 +48,11 @@ IF(NOT CMAKE_GO_LINKER_ID_RUN)
   SET(CMAKE_GO_LINKER_ID_RUN 1)
   #TODO
   SET(CMAKE_LINKER_IS_Linux 1)
-  SET(CMAKE_LINKER_IS_GNUGo 1)
+  SET(CMAKE_LINKER_IS_GNUGO 1)
 ENDIF(NOT CMAKE_GO_COMPILER_ID_RUN)
 
 INCLUDE(CMakeFindBinUtils)
 # configure all variables set in this file
-CONFIGURE_FILE(${CMAKE_MODULE_PATH}/CMakeGoCompiler.cmake.in ${CMAKE_PLATFORM_INFO_DIR}/CMakeGoCompiler.cmake @ONLY)
+CONFIGURE_FILE(${CMAKE_MODULE_PATH}/CMakeGOCompiler.cmake.in ${CMAKE_PLATFORM_INFO_DIR}/CMakeGOCompiler.cmake @ONLY)
 
 SET(CMAKE_GO_COMPILER_ENV_VAR "GO")
--- module/CMakeTestGOCompiler.cmake
+++ module/CMakeTestGOCompiler.cmake
@@ -1,12 +1,19 @@
 INCLUDE(CMakeTestCompilerCommon)
 
+IF(CMAKE_GO_COMPILER_FORCED)
+  # The compiler configuration was forced by the user.
+  # Assume the user has configured all compiler information.
+  set(CMAKE_GO_COMPILER_WORKS TRUE)
+  return()
+ENDIF(CMAKE_GO_COMPILER_FORCED)
+
 # This file is used by EnableLanguage in cmGlobalGenerator to
-# determine that that selected Go compiler can actually compile
+# determine that that selected GO compiler can actually compile
 # and link the most basic of programs. If not, a fatal error
 # is set and cmake stops processing commands and will not generate
 # any makefiles or projects.
 IF(NOT CMAKE_GO_COMPILER_WORKS)
-  PrintTestCompilerStatus("Go" "")
+  PrintTestCompilerStatus("GO" "")
   FILE(WRITE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/cmTryCompileExec.go
     "package main\n"
     "import (\"fmt\")\n"
@@ -15,40 +22,36 @@ IF(NOT CMAKE_GO_COMPILER_WORKS)
     "  return\n"
     "}\n")
   TRY_COMPILE(CMAKE_GO_COMPILER_WORKS ${CMAKE_BINARY_DIR}
-    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/cmTryCompileExec.adb
+    ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/main.go
     OUTPUT_VARIABLE __CMAKE_GO_COMPILER_OUTPUT)
-  SET(Go_TEST_WAS_RUN 1)
+  SET(GO_TEST_WAS_RUN 1)
 ENDIF(NOT CMAKE_GO_COMPILER_WORKS)
 
 IF(NOT CMAKE_GO_COMPILER_WORKS)
-  PrintTestCompilerStatus("Go" " -- broken")
+  PrintTestCompilerStatus("GO" " -- broken")
   FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
-    "Determining if the Go compiler works failed with "
+    "Determining if the GO compiler works failed with "
     "the following output:\n${__CMAKE_GO_COMPILER_OUTPUT}\n\n")
-  MESSAGE(FATAL_ERROR "The Go compiler \"${CMAKE_GO_COMPILER}\" "
+  MESSAGE(FATAL_ERROR "The GO compiler \"${CMAKE_GO_COMPILER}\" "
     "is not able to compile a simple test program.\nIt fails "
     "with the following output:\n ${__CMAKE_GO_COMPILER_OUTPUT}\n\n"
     "CMake will not be able to correctly generate this project.")
 ELSE(NOT CMAKE_GO_COMPILER_WORKS)
-  PrintTestCompilerStatus("Go" " -- works")
+  PrintTestCompilerStatus("GO" " -- works")
   FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
-    "Determining if the Go compiler works passed with "
+    "Determining if the GO compiler works passed with "
     "the following output:\n${__CMAKE_GO_COMPILER_OUTPUT}\n\n")
   SET(CMAKE_GO_COMPILER_WORKS 1 CACHE INTERNAL "")
 
-  IF(CMAKE_GO_COMPILER_FORCED)
-    # The compiler configuration was forced by the user.
-    # Assume the user has configured all compiler information.
-  ELSE(CMAKE_GO_COMPILER_FORCED)
-    # Try to identify the ABI and configure it into CMakeGoCompiler.cmake
-    INCLUDE(${CMAKE_ROOT}/Modules/CMakeDetermineCompilerABI.cmake)
-    CMAKE_DETERMINE_COMPILER_ABI(Go ${CMAKE_MODULE_PATH}/CMakeGoCompilerABI.adb)
-    CONFIGURE_FILE(
-      ${CMAKE_MODULE_PATH}/CMakeGoCompiler.cmake.in
-      ${CMAKE_PLATFORM_INFO_DIR}/CMakeGoCompiler.cmake
-    )
-    INCLUDE(${CMAKE_PLATFORM_INFO_DIR}/CMakeGoCompiler.cmake)
-  ENDIF(CMAKE_GO_COMPILER_FORCED)
+  # Try to identify the ABI and configure it into CMakeGOCompiler.cmake
+  INCLUDE(${CMAKE_ROOT}/Modules/CMakeDetermineCompilerABI.cmake)
+  CMAKE_DETERMINE_COMPILER_ABI(GO ${CMAKE_MODULE_PATH}/CMakeGOCompilerABI.adb)
+  CONFIGURE_FILE(
+    ${CMAKE_MODULE_PATH}/CMakeGOCompiler.cmake.in
+    ${CMAKE_PLATFORM_INFO_DIR}/CMakeGOCompiler.cmake
+    @ONLY
+  )
+  INCLUDE(${CMAKE_PLATFORM_INFO_DIR}/CMakeGOCompiler.cmake)
 ENDIF(NOT CMAKE_GO_COMPILER_WORKS)
 
 UNSET(__CMAKE_GO_COMPILER_OUTPUT)
